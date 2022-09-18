2aa58d6a26346b2335f0e8f953638e48f4803a12
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2022-09-18 22:48:26 +0300

    MT-0006: Добавление в проект ZipFileModel

---
+++ Mt.Utilities/IO/ZipModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Mt.Utilities.IO
+{
+    /// <summary>
+    /// Модель zip-файла 
+    /// </summary>
+    public sealed class ZipFileModel : FileModel
+    {
+        /// <summary>
+        /// Инициализация экземпляра класса <see cref="ZipFileModel"/>.
+        /// </summary>
+        /// <param name="title">Наименование файла.</param>
+        /// <param name="bytes">Данные файла в бинарном формате.</param>
+        public ZipFileModel(string title, IEnumerable<byte> bytes) : base($"{title}.zip", bytes)
+        {
+        }
+    }
+}
